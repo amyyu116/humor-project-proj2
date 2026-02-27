986611d9e41d91de174a8421989620d0d3e178fa
Amy Yu <amyyu116@gmail.com>
2026-02-26 23:49:52 -0500

    fix npm build

--- app/components/PackedBubbleChart.tsx
+++ app/components/PackedBubbleChart.tsx
@@ -27,55 +27,62 @@ export default function PackedBubbleChart({ images }: { images: ImageType[] }) {
 
         const hierarchy = d3.hierarchy(data).sum((d: any) => d.value);
 
-        return d3.pack().size([size, size]).padding(4)(hierarchy);
+        return d3.pack().size([size, size]).padding(4)(hierarchy as any);
     }, [images]);
 
     return (
         <div style={{ position: "relative" }}>
             <svg width={size} height={size}>
-                {root.leaves().map((node, i) => (
-                    <g
-                        key={i}
-                        transform={`translate(${node.x},${node.y})`}
-                        onMouseEnter={(e) =>
-                            setTooltip({
-                                x: e.clientX,
-                                y: e.clientY,
-                                data: node.data,
-                            })
-                        }
-                        onMouseMove={(e) =>
-                            setTooltip((prev: any) =>
-                                prev
-                                    ? { ...prev, x: e.clientX, y: e.clientY }
-                                    : null,
-                            )
-                        }
-                        onMouseLeave={() => setTooltip(null)}
-                        style={{ cursor: "pointer" }}
-                    >
-                        <clipPath id={`clip-${i}`}>
-                            <circle r={node.r} />
-                        </clipPath>
+                {root.leaves().map((node, i) => {
+                    const data = node.data as ImageType;
+                    return (
+                        <g
+                            key={i}
+                            transform={`translate(${node.x},${node.y})`}
+                            onMouseEnter={(e) =>
+                                setTooltip({
+                                    x: e.clientX,
+                                    y: e.clientY,
+                                    data: node.data,
+                                })
+                            }
+                            onMouseMove={(e) =>
+                                setTooltip((prev: any) =>
+                                    prev
+                                        ? {
+                                              ...prev,
+                                              x: e.clientX,
+                                              y: e.clientY,
+                                          }
+                                        : null,
+                                )
+                            }
+                            onMouseLeave={() => setTooltip(null)}
+                            style={{ cursor: "pointer" }}
+                        >
+                            <clipPath id={`clip-${i}`}>
+                                <circle r={node.r} />
+                            </clipPath>
 
-                        <image
-                            href={node.data.url}
-                            width={node.r * 2}
-                            height={node.r * 2}
-                            x={-node.r}
-                            y={-node.r}
-                            clipPath={`url(#clip-${i})`}
-                            preserveAspectRatio="xMidYMid slice"
-                        />
+                            <image
+                                href={data.url}
+                                width={node.r * 2}
+                                height={node.r * 2}
+                                x={-node.r}
+                                y={-node.r}
+                                clipPath={`url(#clip-${i})`}
+                                preserveAspectRatio="xMidYMid slice"
+                            />
 
-                        <circle
-                            r={node.r}
-                            fill="none"
-                            stroke="white"
-                            strokeWidth={1.5}
-                        />
-                    </g>
-                ))}
+                            <circle
+                                r={node.r}
+                                fill="none"
+                                stroke="white"
+                                strokeWidth={1.5}
+                            />
+                        </g>
+                    );
+                })}
             </svg>
 
             {tooltip && (
